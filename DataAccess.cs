0d965fcaf41068766f2b251997dde351e3da8645
Sean Pelser <sean.pelser97@gmail.com>
2023-05-13 17:21:32 +0100

    Create DataAccess.cs

---
+++ DataAccess.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace SQLProject
+{
+    class DataAccess
+    {
+        private static string cnnStr = "";
+
+        public static List<T> GetData<T>(string sql)
+        {
+            try
+            {
+                using (IDbConnection conn = new SqlConnection(cnnStr))
+                {
+                    return conn.Query<T>(sql).ToList();
+                }
+            }
+
+            catch(Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+        }
+
+        public static List<T> GetDataProc<T>(string sql, object? param = null)
+        {
+            try
+            {
+                using (IDbConnection conn = new SqlConnection(cnnStr))
+                {
+                    return conn.Query<T>(sql, param, commandType: CommandType.StoredProcedure).ToList();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+        }
+
+        public static void SQLQuery(string s)
+        {
+            try
+            {
+                using (IDbConnection conn = new SqlConnection(cnnStr))
+                {
+                    conn.Execute(s);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+
+        }
+    }
+}
